7fd2c1f2b89e9478f30cd60c1af445bdf9b676a7
Jesus Rueda <jerue@uv.es>
2017-05-31 11:38:44 +0200

    Make RefTeX work for sure

--- custom.el
+++ custom.el
@@ -31,13 +31,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(LaTeX-mode-hook
-   (quote
-    (preview-mode-setup turn-on-reftex
-                        (lambda nil
-                          (run-hooks
-                           (quote prelude-latex-mode-hook)))
-                        er/add-latex-mode-expansions)))
  '(TeX-PDF-via-dvips-ps2pdf t)
  '(TeX-auto-save t)
  '(TeX-command-list
@@ -366,6 +359,11 @@
 ;;---------------------------------------------------------------------
 ;; new keywords
 (font-lock-add-keywords 'f90-mode '(("\\<\\(dlog\\|dexp\\|dlog10\\|dsqrt\\|dabs\\|isnan\\|dmax1\\|dmin1\\|idnint\\)\\>" . font-lock-builtin-face)))
+
+;;---------------------------------------------------------------------
+;; LaTeX
+;;---------------------------------------------------------------------
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
 
 ;;---------------------------------------------------------------------
 ;; GNUplot
