dc723dbafcd6d70efa86d1042dfbcfd6090bfe4a
Jesus Rueda <jesus@ifm.umich.mx>
2018-03-06 11:37:44 -0600

    Including snazzy theme and disabling the server

--- custom2.el
+++ custom2.el
@@ -11,28 +11,64 @@
  '(TeX-PDF-via-dvips-ps2pdf t)
  '(auto-dim-other-buffers-mode t)
  '(backup-directory-alist (quote ((".*" . "/Users/jesus/.ebackups/"))))
+ '(blink-cursor-mode nil)
  '(calendar-date-style (quote european))
+ '(column-number-mode t)
  '(compilation-message-face (quote default))
  '(confirm-kill-emacs (quote yes-or-no-p))
+ '(custom-enabled-themes (quote (snazzy smart-mode-line-respectful)))
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "d9850d120be9d94dd7ae69053630e89af8767c36b131a3aa7b06f14007a24656" "0809485f08aa8c9b0100033eaa2d04f6a7410c2afcdbd76ce368a7a8e5744ffb" default)))
+ '(custom-theme-allow-multiple-selections t)
  '(f90-mode-hook (quote (f90-add-imenu-menu abbrev-mode auto-fill-mode)))
  '(f90-program-indent 0)
+ '(fci-rule-color "#383838")
  '(fill-column 75)
  '(font-latex-fontify-script nil)
  '(fortran-line-length 1000)
  '(indicate-empty-lines t)
  '(mouse-wheel-scroll-amount (quote (1 ((shift) . 1) ((control)))))
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
  '(package-selected-packages
    (quote
-    (neotree tabbar auto-dim-other-buffers geiser json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company smex ido-completing-read+ flx-ido vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
+    (snazzy-theme neotree tabbar auto-dim-other-buffers geiser json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company smex ido-completing-read+ flx-ido vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(prelude-whitespace nil)
  '(python-shell-interpreter "ipython")
  '(python-shell-interpreter-args "--simple-prompt -i --automagic")
  '(scroll-bar-mode nil)
  '(show-paren-style (quote mixed))
+ '(size-indication-mode t)
  '(tabbar-mode t nil (tabbar))
  '(tabbar-use-images nil)
+ '(tool-bar-mode nil)
  '(tramp-default-method "sshx")
- '(user-mail-address "jesus.rueda@uv.es"))
+ '(user-mail-address "jesus.rueda@uv.es")
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -94,7 +130,7 @@
 ;;---------------------------------------------------------------------
 ;; Start Server
 ;;---------------------------------------------------------------------
-(server-start)
+;; (server-start)
 
 ;; define function to shutdown emacs server instance
 (defun server-shutdown ()
