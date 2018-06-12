04f7717a60625588cf9d4e6d19093da2f158ad94
jerue <jesus@draco6.ifm.umich.mx>
2018-06-12 12:58:50 -0500

    init file for dracos

---
+++ custom_dracos.el
@@ -0,0 +1,148 @@
+;;; custom.el --- Summary
+;;; Commentary:
+;;; Code:
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(LaTeX-command "latex -synctex=1")
+ '(TeX-PDF-via-dvips-ps2pdf t)
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["#3F3F3F" "#CC9393" "#7F9F7F" "#F0DFAF" "#8CD0D3" "#DC8CC3" "#93E0E3" "#DCDCCC"])
+ '(ansi-term-color-vector
+   [unspecified "#1e1f29" "#ff5c57" "#5af78e" "#f3f99d" "#57c7ff" "#ff6ac1" "#57c7ff" "#eff0eb"])
+ '(auto-dim-other-buffers-mode t)
+ '(backup-directory-alist (quote ((".*" . "/Users/jesus/.ebackups/"))))
+ '(calendar-date-style (quote european))
+ '(column-number-mode t)
+ '(company-quickhelp-color-background "#4F4F4F")
+ '(company-quickhelp-color-foreground "#DCDCCC")
+ '(compilation-message-face (quote default))
+ '(confirm-kill-emacs (quote yes-or-no-p))
+ '(cursor-type (quote bar))
+ '(custom-enabled-themes (quote (base16-snazzy smart-mode-line-respectful)))
+ '(custom-safe-themes
+   (quote
+    ("d80975e30569ad6c5f359521eac1b32626015aed1c8bef139c96418a38dd3b04" "e11569fd7e31321a33358ee4b232c2d3cf05caccd90f896e1df6cab228191109" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "0809485f08aa8c9b0100033eaa2d04f6a7410c2afcdbd76ce368a7a8e5744ffb" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
+ '(custom-theme-allow-multiple-selections t)
+ '(f90-mode-hook (quote (f90-add-imenu-menu abbrev-mode auto-fill-mode)))
+ '(fci-rule-color "#383838")
+ '(fill-column 80)
+ '(font-latex-fontify-script nil)
+ '(fortran-line-length 1000)
+ '(global-hl-line-mode nil)
+ '(indicate-empty-lines t)
+ '(mouse-wheel-scroll-amount (quote (1 ((shift) . 1) ((control)))))
+ '(nrepl-message-colors
+   (quote
+    ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
+ '(package-selected-packages
+   (quote
+    (minimap base16-theme helm snazzy-theme neotree tabbar auto-dim-other-buffers geiser json-mode js2-mode rainbow-mode elisp-slime-nav rainbow-delimiters company smex ido-completing-read+ flx-ido vkill exec-path-from-shell zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
+ '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
+ '(prelude-whitespace nil)
+ '(python-shell-interpreter "ipython")
+ '(python-shell-interpreter-args "--simple-prompt -i --automagic")
+ '(scroll-bar-mode nil)
+ '(show-paren-style (quote mixed))
+ '(size-indication-mode t)
+ '(tabbar-use-images nil)
+ '(tramp-default-method "sshx")
+ '(user-mail-address "jesus.rueda@uv.es")
+ '(vc-annotate-background "#2B2B2B")
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#BC8383")
+     (40 . "#CC9393")
+     (60 . "#DFAF8F")
+     (80 . "#D0BF8F")
+     (100 . "#E0CF9F")
+     (120 . "#F0DFAF")
+     (140 . "#5F7F5F")
+     (160 . "#7F9F7F")
+     (180 . "#8FB28F")
+     (200 . "#9FC59F")
+     (220 . "#AFD8AF")
+     (240 . "#BFEBBF")
+     (260 . "#93E0E3")
+     (280 . "#6CA0A3")
+     (300 . "#7CB8BB")
+     (320 . "#8CD0D3")
+     (340 . "#94BFF3")
+     (360 . "#DC8CC3"))))
+ '(vc-annotate-very-old-color "#DC8CC3"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 110 :family "DejaVu Sans Mono"))))
+ '(auto-dim-other-buffers-face ((t (:foreground "AntiqueWhite4")))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;---------------------------------------------------------------------
+;; Key bindings
+;;---------------------------------------------------------------------
+(global-set-key (kbd "s-w") 'kill-this-buffer)
+;; (global-set-key (kbd "C-x C-c") 'delete-frame)
+
+;;---------------------------------------------------------------------
+;; Fortran 90 programming
+;;---------------------------------------------------------------------
+;; new keywords
+(font-lock-add-keywords 'f90-mode '(("\\<\\(dlog\\|dexp\\|dlog10\\|dsqrt\\|dabs\\|isnan\\|dmax1\\|dmin1\\|idnint\\)\\>" . font-lock-builtin-face)))
+
+;;---------------------------------------------------------------------
+;; LaTeX
+;;---------------------------------------------------------------------
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+
+;;---------------------------------------------------------------------
+;; GNUplot
+;;---------------------------------------------------------------------
+(setq auto-mode-alist (append '(("\\.gp$" . gnuplot-mode))
+                              auto-mode-alist))
+
+;;---------------------------------------------------------------------
+;; Tabbar
+;;---------------------------------------------------------------------
+(global-set-key [M-s-left] 'tabbar-backward-tab)
+(global-set-key [M-s-right] 'tabbar-forward-tab)
+(global-set-key (kbd "s-b") 'tabbar-press-home)
+;; Activate NeoTree
+(global-set-key [f8] 'neotree-toggle)
+
+(setq tabbar-buffer-groups-function
+      (lambda ()
+        (let ((dir (expand-file-name default-directory)))
+          (cond ((member (buffer-name) '("*Completions*"
+                                         "*scratch*"
+                                         "*Messages*"
+                                         "*Ediff Registry*"))
+                 (list "#Emacs"))
+                ;;                ((string-match-p "^\\*.+\\*$" (buffer-name))
+                ((string-match-p "^\\*.+$" (buffer-name))
+                 (list "#Rest"))
+                ((string-match-p "/.emacs.d/" dir)
+                 (list ".emacs.d"))
+                ((string-match-p "\*.?Python\[?.*\]?\*" dir)
+                 (list "#Python"))
+                (t (list dir))))))
+
+;;---------------------------------------------------------------------
+;; Start Server
+;;---------------------------------------------------------------------
+;; (server-start)
+
+;; define function to shutdown emacs server instance
+(defun server-shutdown ()
+  "Save buffers, Quit, and Shutdown (kill) server"
+  (interactive)
+  (save-some-buffers)
+  (kill-emacs))
+;;; custom.el ends here
